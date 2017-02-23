ff80776926b3f8d12e73aa4f15dbc9cfa0bbc322
Amrutha Gujjar <amruthasg@yahoo.com>
2017-02-22 17:22:29 -0800

    fixing some formatting stuff

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 angular.module('tedx-app', [])
 	.controller('AppController', function($scope) {
 		$scope.sections = [{text:'Schedule'}, {text:'Speakers'}, {text:'Activities'}];
+
+	})
+	.controller('ScheduleController', function($scope) {
+		$scope.speakers = [{text:'Speaker1'}, {text: 'Speaker2'}];
 	});
