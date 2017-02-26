be5c5bb45d958083c33cb75d08f191d669d9293c
anirmal <Aishwarya@Aishwaryas-MacBook-Pro-2.local>
2017-02-26 14:39:49 -0800

    red

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
+// Angular Javascript file
+
 angular.module('tedx-app', [])
 	.controller('AppController', function($scope) {
-		$scope.sections = [{text:'Schedule', content:'schedule-scroll'}, 
-						   {text:'Speakers', content:'speakers-scroll'}, 
-						   {text:'Activities', content: 'activities-scroll'}];
+		// Overall home page controller
+		$scope.sections = [{text:'Schedule', content:'schedule-scroll', insert:'ScheduleSection'}, 
+						   {text:'Speakers', content:'speakers-scroll', insert:'SpeakerSection'}, 
+						   {text:'Activities', content: 'activities-scroll', insert:'ActivitySection'}];
 
 	})
 	.controller('ScheduleController', function($scope) {
+		// Schedule page controller
 		$scope.speakers = [{text:'Speaker1'}, {text: 'Speaker2'}];
 	});
