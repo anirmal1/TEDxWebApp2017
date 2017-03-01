2386f89f9d852f7f16c737d945cb4dd46e7fff22
anirmal <Aishwarya@D-69-91-137-55.dhcp4.washington.edu>
2017-03-01 14:13:13 -0800

    eh

--- app.js
+++ app.js
@@ -3,10 +3,30 @@
 angular.module('tedx-app', [])
 	.controller('AppController', function($scope) {
 		// Overall home page controller
-		$scope.sections = [{text:'Schedule', content:'schedule-scroll', insert:'ScheduleSection'}, 
-						   {text:'Speakers', content:'speakers-scroll', insert:'SpeakerSection'}, 
-						   {text:'Activities', content: 'activities-scroll', insert:'ActivitySection'}];
+		$scope.sections = [{text:'Schedule', content:'schedule-scroll', items:'schedule'}, 
+						   {text:'Speakers', content:'speakers-scroll', items:'speakers'}, 
+						   {text:'Activities', content: 'activities-scroll', items:'activities'}];
 
+	})
+	.controller('HomeSectionController', function($scope) {
+		$scope.init = function(type) {
+			if (type === 'schedule') {
+				$scope.items = [{time:'1:30pm', currEvent:'Speaker1'},
+								{time:'2:00pm', currEvent:'Speaker2'},
+								{time:'2:30pm', currEvent:'Speaker3'},
+								{time:'3:00pm', currEvent:'Entertainment1'},
+								{time:'3:30pm', currEvent:'Speaker4'},
+								{time:'4:00pm', currEvent:'Speaker5'},
+								{time:'4:30pm', currEvent:'Entertainment2'}]; // Placeholder values
+				
+			} 
+			if (type == 'speakers') {
+				$scope.items = []
+			} 
+			if (type == 'activities') {
+				$scope.items = []
+			}
+		}
 	})
 	.controller('ScheduleController', function($scope) {
 		// Schedule page controller
