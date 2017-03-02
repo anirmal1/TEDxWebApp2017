55c458c63e71a9b23906ee69b6c652e4f9d3608c
anirmal <Aishwarya@D-69-91-137-55.dhcp4.washington.edu>
2017-03-01 20:30:25 -0800

    no angular

--- app.js
+++ app.js
@@ -1,30 +1,39 @@
-// Angular Javascript file
+(window.onload = function() {
+	// Schedule information for home page
 
-angular.module('tedx-app', [])
-	.controller('AppController', function($scope) {
-		// Overall home page controller
-		$scope.sections = [{text:'Schedule', 
-							content:'schedule-scroll', 
-							items: [{time:'1:30pm', currEvent:'Speaker1'},
-									{time:'2:00pm', currEvent:'Speaker2'},
-									{time:'2:30pm', currEvent:'Speaker3'},
-									{time:'3:00pm', currEvent:'Entertainment1'},
-									{time:'3:30pm', currEvent:'Speaker4'},
-									{time:'4:00pm', currEvent:'Speaker5'},
-									{time:'4:30pm', currEvent:'Entertainment2'}], 
-									controller:'ScheduleSectionController'}, 
-						   {text:'Speakers', 
-						   content:'speakers-scroll', 
-						   items: [], 
-						   controller:'SpeakerSectionController'}, 
-						   {text:'Activities', 
-						   content: 'activities-scroll', 
-						   items: [], 
-						   controller:'ScheduleController'}];
+	// Speaker information for home page
 
-	})
+	// Activities information for home page
 
-	.controller('ScheduleController', function($scope) {
-		// Schedule page controller
-		$scope.speakers = [{text:'Speaker1'}, {text: 'Speaker2'}];
-	});
+});
+
+// // Angular Javascript file
+
+// angular.module('tedx-app', [])
+// 	.controller('AppController', function($scope) {
+// 		// Overall home page controller
+// 		$scope.sections = [{text:'Schedule', 
+// 							content:'schedule-scroll', 
+// 							items: [{time:'1:30pm', currEvent:'Speaker1'},
+// 									{time:'2:00pm', currEvent:'Speaker2'},
+// 									{time:'2:30pm', currEvent:'Speaker3'},
+// 									{time:'3:00pm', currEvent:'Entertainment1'},
+// 									{time:'3:30pm', currEvent:'Speaker4'},
+// 									{time:'4:00pm', currEvent:'Speaker5'},
+// 									{time:'4:30pm', currEvent:'Entertainment2'}], 
+// 									controller:'ScheduleSectionController'}, 
+// 						   {text:'Speakers', 
+// 						   content:'speakers-scroll', 
+// 						   items: [], 
+// 						   controller:'SpeakerSectionController'}, 
+// 						   {text:'Activities', 
+// 						   content: 'activities-scroll', 
+// 						   items: [], 
+// 						   controller:'ScheduleController'}];
+
+// 	})
+
+// 	.controller('ScheduleController', function($scope) {
+// 		// Schedule page controller
+// 		$scope.speakers = [{text:'Speaker1'}, {text: 'Speaker2'}];
+// 	});
