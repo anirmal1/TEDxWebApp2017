0d102d3874514a81ed3e0edcf214be13045b98c6
anirmal <Aishwarya@D-69-91-137-55.dhcp4.washington.edu>
2017-03-01 17:00:17 -0800

    fixed items

--- app.js
+++ app.js
@@ -3,26 +3,27 @@
 angular.module('tedx-app', [])
 	.controller('AppController', function($scope) {
 		// Overall home page controller
-		$scope.sections = [{text:'Schedule', content:'schedule-scroll', controller:'ScheduleSectionController'}, 
-						   {text:'Speakers', content:'speakers-scroll', controller:'SpeakerSectionController'}, 
-						   {text:'Activities', content: 'activities-scroll', controller:'ScheduleController'}];
+		$scope.sections = [{text:'Schedule', 
+							content:'schedule-scroll', 
+							items: [{time:'1:30pm', currEvent:'Speaker1'},
+									{time:'2:00pm', currEvent:'Speaker2'},
+									{time:'2:30pm', currEvent:'Speaker3'},
+									{time:'3:00pm', currEvent:'Entertainment1'},
+									{time:'3:30pm', currEvent:'Speaker4'},
+									{time:'4:00pm', currEvent:'Speaker5'},
+									{time:'4:30pm', currEvent:'Entertainment2'}], 
+									controller:'ScheduleSectionController'}, 
+						   {text:'Speakers', 
+						   content:'speakers-scroll', 
+						   items: [], 
+						   controller:'SpeakerSectionController'}, 
+						   {text:'Activities', 
+						   content: 'activities-scroll', 
+						   items: [], 
+						   controller:'ScheduleController'}];
 
 	})
-	.controller('ScheduleSectionController', function($scope) {
-		$scope.items = [{time:'1:30pm', currEvent:'Speaker1'},
-								{time:'2:00pm', currEvent:'Speaker2'},
-								{time:'2:30pm', currEvent:'Speaker3'},
-								{time:'3:00pm', currEvent:'Entertainment1'},
-								{time:'3:30pm', currEvent:'Speaker4'},
-								{time:'4:00pm', currEvent:'Speaker5'},
-								{time:'4:30pm', currEvent:'Entertainment2'}]; // Placeholder values
-	})
-	.controller('SpeakerSectionController', function($scope) {
-		$scope.items = []
-	})
-	.controller('ActivitiesSectionController', function($scope) {
 
-	})
 	.controller('ScheduleController', function($scope) {
 		// Schedule page controller
 		$scope.speakers = [{text:'Speaker1'}, {text: 'Speaker2'}];
