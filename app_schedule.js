170a0da6adbe2ba010d2ea698b78a2ae9b64d6f7
anirmal <Aishwarya@D-173-250-190-215.dhcp4.washington.edu>
2017-03-07 17:16:31 -0800

    edits to schedule page

---
+++ app_schedule.js
@@ -0,0 +1,23 @@
+// JavaScript file for schedule.html
+
+(window.onload = function() {
+	displaySpeakersList();
+
+	function displaySpeakersList() {
+		var speakers = ['Speaker1', 'Speaker2', 'Speaker3', 'Entertainment1', 'Speaker4', 'Speaker5', 'Speaker6', 'Entertainment2'];
+		speakerList = document.getElementById('speaker-page-list');
+
+		for (var i = 0; i < speakers.length; i++) {
+			var divSpeaker = document.createElement("div");
+			divSpeaker.classList.add("speaker-div-schedule");
+			var speakerName = document.createElement("h1");
+			speakerName.classList.add("speaker-title");
+			speakerName.innerHTML = speakers[i];
+			divSpeaker.appendChild(speakerName);
+			speakerList.appendChild(divSpeaker);
+		}
+	
+	}
+
+});
+
