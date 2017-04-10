eadda3d9e92c6058b55e68f0c02bc0bff35a9d31
anirmal <Aishwarya@Aishwaryas-MacBook-Pro-2.local>
2017-04-10 00:30:51 -0700

    form isnt working, so basically only small edits

--- app.js
+++ app.js
@@ -10,7 +10,18 @@ window.onload = function() {
 	var interactiveHeading = document.createElement('form');
 	interactiveHeading.classList.add('interactive-heading');
 	interactiveHeading.innerHTML = 'yes and ';
-	interactiveHeading.onsubmit = sendYesAndData;
+	
+	// var clientId = '580857203762-8a7l6uad0242olp3lqcat5tiuuksdr26.apps.googleusercontent.com';
+	// if (!/^([0-9])$/.test(clientId[0])) {
+ //    	alert('Invalid Client ID - did you forget to insert your application Client ID?');
+ //    }
+
+	// var realtimeUtils = new utils.RealtimeUtils({ clientId: clientId });
+	// authorize(realtimeUtils);
+	interactiveHeading.onsubmit = function() {
+		// sendYesAndData(realtimeUtils);
+		sendYesAndData();
+	};
 
 	var inputPart = document.createElement('input');
 	inputPart.setAttribute('type', 'text');
@@ -25,28 +36,44 @@ window.onload = function() {
 
 	// Speaker information for home page
 	addSpeakerImages();
-	/*var speakerScroll = document.getElementById('container-scroll');
-	var speakers = ['Speaker1', 'Speaker2', 'Speaker3', 'Speaker4', 'Speaker5', 'Speaker6', 'Speaker7', 'Speaker8'];
-	speakerScroll.style.width = (300 * events.length) + "px"
-	var dirname = "assets/Images2016";
-	var images = glob($dirname."*.jpg");
-	foreach($images as $image) {
-	    echo '<img src="'.$image.'" /><br />';
-	}
-	speakers.forEach(function(loadImage) {
-
-	});*/
 	
 	// Activities information for home page
 };
 
+// function authorize(realtimeUtils) {
+//     // Attempt to authorize
+//     realtimeUtils.authorize(function(response){
+// 	    if(response.error){
+// 	        realtimeUtils.authorize(function(response){
+// 	          }, true);
+// 	    }
+//    	}, false);
+// }
+
 function sendYesAndData() {
-	var yesAndInput = document.getElementById('yesAnd').value;
-	// FIGURE OUT HOW TO DO THIS
-	//var xhr = new XMLHttpRequest();
-	//xhr.open('GET', 'word_cloud_data.php?yesAnd=' + yesAndInput, true); //could change this to POST later?
-	//xhr.send();
+// function sendYesAndData(realtimeUtils) {
+// 	var id = realtimeUtils.getParam('id');
+//     if (id) {
+//       // Load the document id from the URL
+//       realtimeUtils.load(id.replace('/', ''), onDataLoad, onDataInitialize);
+//     } else {
+//       // Create a new document, add it to the URL
+//       realtimeUtils.createRealtimeFile('New Quickstart File', function(createResponse) {
+//         window.history.pushState(null, null, '?id=' + createResponse.id);
+//         realtimeUtils.load(createResponse.id, onDataLoad, onDataInitialize);
+//       });
+//     }
 }
+
+// function onDataLoad(doc) {
+// 	var list = doc.getModel().getRoot().get('yesAndList');
+// 	console.log(list);
+// }
+
+// function onDataInitialize(model) {
+// 	var list = model.createList();
+// 	model.getRoot().set('yesAndList', list);
+// }
 
 function addHomePageScheduleInfo() {
 	var scheduleScrollSection = document.getElementById('schedule-blocks');
@@ -83,7 +110,7 @@ function processSpeakerData() {
 		var imageBlock = document.createElement('img');
 		var path = speakerNames[name].image;
 		imageBlock.setAttribute('src', 'assets/Images2016/' + path);
-		
+
 		// TODO figure out why width is NaN (think it might have to do with style of auto width in CSS)
 		totalWidth += parseInt(imageBlock.style.width);
 		console.log(imageBlock.style.width);
