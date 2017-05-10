6e4f6b1f34f4a25ff6e90ed95c9c69fb9857334e
anirmal <Aishwarya@Aishwaryas-MacBook-Pro-2.local>
2017-05-10 01:27:23 -0700

    overall speakers page edits

---
+++ app_speakers.js
@@ -0,0 +1,32 @@
+(function() {
+window.onload = function() {
+	addSpeakerImages();
+};
+
+function addSpeakerImages() {
+	var xhr = new XMLHttpRequest();
+	xhr.onload = processSpeakerData;
+	xhr.open('GET', 'data/speakers.json', true);
+	xhr.send();
+}
+
+function processSpeakerData() {
+	var data = JSON.parse(this.responseText);
+	var speakerDiv = document.getElementById('speaker-images');
+	var speakerNames = data.speakerNames;
+	for (var name in speakerNames) {
+		var image = document.createElement('img');
+		image.classList.add('speaker-page-image');
+		var path = speakerNames[name].image;
+		image.setAttribute('src', 'assets/Speakers2017/' + path);
+		var link = document.createElement('a');
+		var linkPath = speakerNames[name].link;
+		link.setAttribute('href', 'bio?speaker='+linkPath);
+		link.setAttribute('id', name);
+		link.classList.add('speaker-page-image');
+		link.appendChild(image);
+		speakerDiv.appendChild(link);
+	}
+}
+
+})();
