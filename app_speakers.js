99fd34c1865ffc7a271825addedd65a1722dede6
anirmal <Aishwarya@Aishwaryas-MacBook-Pro-2.local>
2017-05-24 00:42:07 -0700

    speaker titles on speaker page

--- app_speakers.js
+++ app_speakers.js
@@ -21,13 +21,16 @@ function processSpeakerData() {
 		image.setAttribute('src', 'assets/Speakers2017/' + path);
 		var nameCaption = document.createElement('div');
 		nameCaption.classList.add('name-caption');
+		nameCaption.innerHTML = name;
 		var link = document.createElement('a');
 		var linkPath = speakerNames[name].link;
 		link.setAttribute('href', 'bio.html?speaker='+linkPath);
 		link.setAttribute('id', name);
 		link.classList.add('speaker-page-image');
-		link.appendChild(image);
-		link.appendChild(nameCaption);
+		var temp = document.createElement('div');
+		temp.appendChild(image);
+		temp.appendChild(nameCaption);
+		link.appendChild(temp);
 		speakerDiv.appendChild(link);
 	}
 }
